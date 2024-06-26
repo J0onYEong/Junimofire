bfd3f17f7f04fa1354d31dd990ae72052b70fb70
J0onYEong <cjy2134@naver.com>
2024-06-26 22:33:28 +0900

    [Release: 0.2.0] Session기본 interceptor기능 추가

--- Sources/JunimoFire/Session/JuniosSession.swift
+++ Sources/JunimoFire/Session/JuniosSession.swift
@@ -14,30 +14,51 @@ public class JuniosSession {
     let session: URLSession
     let sessionDelegate: URLSessionDelegate
     
+    private var adapters: [JFRequestAdpater]
+    private var retriers: [JFRequestRetrier]
+    
+    private var vaildators: [JFValidation] = []
+    
     public init(
         configuration: URLSessionConfiguration,
-        sessionDelegate: URLSessionDelegate = JuniosSessionDelegate()) {
+        sessionDelegate: URLSessionDelegate = JuniosSessionDelegate(),
+        adapters: [JFRequestAdpater] = [],
+        retriers: [JFRequestRetrier] = [],
+        validators: [JFValidation] = []
+    ) {
             
         let queue = OperationQueue()
         queue.maxConcurrentOperationCount = 1
         queue.qualityOfService = .default
         self.session = URLSession(configuration: configuration, delegate: sessionDelegate, delegateQueue: queue)
         self.sessionDelegate = sessionDelegate
+        self.adapters = adapters
+        self.retriers = retriers
+        self.vaildators = validators
     }
-    
 }
-
 
 // MARK: - data request
 public extension JuniosSession {
     
-    func request(request: URLRequest, interceptor: JFRequestInterceptor?) -> JFDataRequest {
+    func request(request: URLRequest, interceptor: JFRequestInterceptor? = nil) async -> JFDataRequest {
+        
+        // 세션 어뎁터&리트라이어 적용
+        var willAddintercepator = interceptor
+        
+        willAddintercepator?.adapters.append(contentsOf: adapters)
+        willAddintercepator?.retriers.append(contentsOf: retriers)
         
         let dataRequest = JFDataRequest(
             request: request,
-            interceptor: interceptor,
+            interceptor: willAddintercepator,
             session: session
         )
+        
+        // Validation설정
+        for validator in vaildators {
+            _ = await dataRequest.validate(validator: validator)
+        }
         
         return dataRequest
     }
